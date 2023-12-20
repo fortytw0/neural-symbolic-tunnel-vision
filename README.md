da097f1dfdd23ae586bb7fe595b1ccbece51d27d
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2023-12-20 11:37:36 +0530

    LOTR : It's done.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # neural-symbolic-tunnel-vision
-This repository was created so that I finish my work on time, rather than tweak code to make it look pretty. God help me 
+This repository was created so that I finish my work on time, rather than tweak code to make it look pretty. God help me. 
+
+
+## Running Lonformer 
+
+Please refer to `src/longformer.py` to set your hyperparameters and run Longformer. 
+
+## Running BERT 
+
+Please refer to `src/bert.py` to set your hyperparameters and run BERT. 
+
+## Identifying co-referents and finding adjacency matrices
+
+Please refer to `notebooks/coref-resolver.ipynb`
+
+## Running GNN 
+
+Please refer to `notebooks/graph_nn.ipynb`
+
+
+
--- src/bert.py
+++ src/bert.py
@@ -1,7 +1,10 @@
 import os
 import pickle
 import json
-from transformers import LongformerModel, AutoTokenizer
+import numpy as np
+from nltk.tokenize import sent_tokenize
+from tqdm import tqdm
+from transformers import BertModel, AutoTokenizer
 
 '''
 Variable Declaration
@@ -10,16 +13,20 @@ Variable Declaration
 train_data_path = 'data/docnli/train_5sent_50ksample.json'
 dev_data_path = 'data/docnli/dev_5sent_10ksample.json'
 
-train_data_output_dir = 'data/longformer/train'
-dev_data_output_dir = 'data/longformer/dev'
+save_dir = 'data/bert'
+train_data_output_dir = 'data/bert/train'
+dev_data_output_dir = 'data/bert/dev'
 batch_size = 2
+
+model_name = 'bert-base-cased'
+tokenizer_name = 'bert-base-cased'
 
 '''
 Folder Creation
 '''
 
-if not os.path.exists('data/longformer') : 
-    os.mkdir('data/longformer')
+if not os.path.exists(save_dir) : 
+    os.mkdir(save_dir)
 
 if not os.path.exists(train_data_output_dir) : 
     os.mkdir(train_data_output_dir)
@@ -32,8 +39,8 @@ if not os.path.exists(dev_data_output_dir) :
 Load Models
 ''' 
 
-model = LongformerModel.from_pretrained("allenai/longformer-base-4096")
-tokenizer = AutoTokenizer.from_pretrained("allenai/longformer-base-4096")
+model = LongformerModel.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 
 model = model.to("cuda")
 
@@ -57,6 +64,18 @@ def save_dict_as_pickle(d , save_path) :
     with open(save_path, 'wb') as f : 
         pickle.dump(d, f)
 
+def get_cls(text) : 
+
+    tokens = tokenizer(text, padding='max_length' , truncation=True, return_tensors='pt')
+    
+    input_ids = tokens.input_ids.to("cuda")
+    attention_mask = tokens.attention_mask.to("cuda")
+
+    cls = model(input_ids=input_ids, attention_mask=attention_mask)[:,0,:].cpu().detach().numpy()
+
+    return cls
+
+
 '''
 Train Loop
 '''
@@ -65,25 +84,33 @@ start_index = int(len(os.listdir(train_data_output_dir))/batch_size)
 try : 
     train_batches = [train_batch for train_batch in batch(train_data, batch_size)][start_index : ]
 
-    for data in train_batches : 
+    for i , data in tqdm(enumerate(train_batches)) : 
 
-        premise_hypothesis_pairs = [(d['premise'] , d['hypothesis']) for d in data]
 
-        tokens = tokenizer(premise_hypothesis_pairs, padding='max_length' , truncation=True, return_tensors='pt')
+        premise = [sent_tokenize(d['premise']) for d in data]
+        hypothesis = [sent_tokenize(d['hypothesis']) for d in data]
+
+
+
+
+
+
         
-        input_ids = tokens.input_ids.to("cuda")
-        attention_mask = tokens.attention_mask.to("cuda")
 
-        longformer_repr = model(input_ids=input_ids, attention_mask=attention_mask)
-        longformer_repr = longformer_repr.last_hidden_state[:,0,:].cpu().detach().numpy()
+
+
+
+        
+        
 
         for i , d in enumerate(data) : 
-            d['longformer_repr'] = longformer_repr[i]
+            d['repr'] = longformer_repr[i]
             save_path = os.path.join(train_data_output_dir, f"{d['id']}.pkl")
             save_dict_as_pickle(d , save_path)
 
 except Exception as e : 
 
+    print(f"Error encountered in Train Loop Batch : {i+1}/{len(train_batches)} ")
     print(e)
 
 '''
@@ -94,7 +121,7 @@ start_index = int(len(os.listdir(dev_data_output_dir))/batch_size)
 try : 
     dev_batches = [dev_batch for dev_batch in batch(dev_data, batch_size)][start_index : ]
 
-    for data in dev_batches : 
+    for i , data in tqdm(enumerate(dev_batches)) : 
 
         premise_hypothesis_pairs = [(d['premise'] , d['hypothesis']) for d in data]
 
@@ -107,10 +134,11 @@ try :
         longformer_repr = longformer_repr.last_hidden_state[:,0,:].cpu().detach().numpy()
 
         for i , d in enumerate(data) : 
-            d['longformer_repr'] = longformer_repr[i]
+            d['repr'] = longformer_repr[i]
             save_path = os.path.join(dev_data_output_dir, f"{d['id']}.pkl")
             save_dict_as_pickle(d , save_path)
 
 except Exception as e : 
 
+    print(f"Error encountered in Val Loop Batch : {i+1}/{len(train_batches)} ")
     print(e)
